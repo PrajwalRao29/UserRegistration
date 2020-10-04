6f8e23741e83e1c3216ea3f43341ac2044cc262d
PrajwalRao29 <raoprajrao@gmail.com>
2020-10-04 17:24:23 +0530

    Added functionality to check first name

---
+++ User.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class User {
+	public static boolean FirstNameCheck() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter First Name");
+		if (!Pattern.matches("[A-Z]{1}[a-z]{2,}", scanner.next())) {
+			System.out.println("Not a valid First Name , Please repeat.");
+			return FirstNameCheck();
+		} else {
+			System.out.println("Valid First Name");
+			return true;
+		}
+	}
+	public static void main(String[] args) {
+
+		FirstNameCheck();
+	}
+}
