603b229ce23499c252345674f1d47f97d2a6776d
PrajwalRao29 <raoprajrao@gmail.com>
2020-10-04 18:27:28 +0530

    added functionality to check for email

--- User.java
+++ User.java
@@ -18,15 +18,30 @@ public class User {
 		System.out.println("Enter Last Name");
 		if (!Pattern.matches("[A-Z]{1}[a-z]{2,}", scanner.next())) {
 			System.out.println("Not a valid Last Name , Please repeat.");
-			return FirstNameCheck();
+			return LastNameCheck();
 		} else {
 			System.out.println("Valid Last Name");
 			return true;
+		}
+	}
+
+	public static boolean emailCheck()
+	{	Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter your email");
+		if(Pattern.matches("[\\w]{2,}([.][a-zA-Z]{2,})*[@]{1}[a-zA-Z]{2,}[.]{1}[a-zA-Z]{2,}([.][a-zA-Z]{2,})*",scanner.next()))
+				{
+			System.out.println("Valid Email");
+			return true;
+				}
+		else {
+			System.out.println("Invalid Email , Please repeat.");
+			return emailCheck();
 		}
 	}
 	public static void main(String[] args) {
 
 		FirstNameCheck();
 		LastNameCheck();
+		emailCheck();
 	}
 }
