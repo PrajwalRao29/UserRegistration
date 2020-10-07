2df3dbd8291ff50a33ec996faabe956256357153
PrajwalRao29 <raoprajrao@gmail.com>
2020-10-07 19:20:06 +0530

    Added parametered testing for email samples

---
+++ Intellij/src/test/java/com/cg/userregistration/PTestUser.java
@@ -0,0 +1,55 @@
+package com.cg.userregistration;
+import com.cg.userregistration.userregistration.User;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import java.util.*;
+
+@RunWith(Parameterized.class)
+public class PTestUser {
+    private static String str;
+    private static boolean expected;
+    private static User user;
+
+    public PTestUser(String str, boolean expected) {
+        this.str = str;
+        this.expected = expected;
+    }
+
+    @Before
+    public void initialize() {
+        user = new User();
+    }
+
+    @Parameterized.Parameters
+    public static Collection input() {
+        return Arrays.asList(new Object[][]{
+                {"abc@yahoo.com", true},
+                {"abc-100@yahoo.com", true},
+                {"abc.100@yahoo.com", true},
+                {"abc111@abc.com", true},
+                {"abc-100@abc.net", true},
+                {"abc.100@abc.com.au", true},
+                {"abc@gmail.com.com", true},
+                {"abc+100@gmail.com", true},
+                {"abc", false},
+                {"abc@.com.my", false},
+                {"abc123@gmail.a", false},
+                {"abc123@ VEmail.put(.com", false},
+                {"abc123@.com.com", false},
+                {".abc@abc.com", false},
+                {"abc()*@gmail.com", false},
+                {"abc@%*.com", false},
+                {"abc..2002@gmail.com", false},
+                {"abc.@gmail.com", false},
+                {"abc@abc@gmail.com", false},
+                {"abc@gmail.com.1a", false},
+                {"abc@gmail.com.aa.au", false}
+        });
+    }
+    @Test
+    public void testEmail_ValidSamples() {
+        Assert.assertEquals(user.emailCheck(str), expected);
+    }
+}
+
